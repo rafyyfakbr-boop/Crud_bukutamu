7929895df45efda56f2bf8c7782446e4207ebadb
Fikri raff <rafyyfakbr@gmail.com>
2026-09-07 14:13:03 +0700

    menambahkan fitur login session dan logout

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,14 @@
+<?php
+// memulai session
+session_start();
+
+// cek bila tidak ada user yang login maka akan di redirect ke halaman login
+if (!isset($_SESSION['login'])) {
+    header('Location: login.php');
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -43,6 +54,16 @@
 
             <!-- Divider -->
             <hr class="sidebar-divider my-0">
+            <?php
+            // cek apabila ada user login maka tampilkan logout
+            if (isset($_SESSION['login'])) :
+            ?>
+            <li class="nav-item">
+                <a class="nav-link" href="logout.php">
+                    <i class="fas fa-fw fa-power-off"></i>
+                    <span>Logout</span></a>
+            </li>
+            <?php endif; ?>
 
             
 
