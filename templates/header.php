767e033f462f5601393db8b4b4f18ebf50bdaf7e
Fikri raff <rafyyfakbr@gmail.com>
2026-09-25 09:12:53 +0700

    fixingakun

--- templates/header.php
+++ templates/header.php
@@ -176,9 +176,16 @@ if (!isset($_SESSION['login'])) {
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+                                    <?= htmlspecialchars($_SESSION['username']) ?>
+                                    <br>
+                                    <small class="text-muted" style="font-size: 11px;">
+                                        <?= ucfirst($_SESSION['role']) ?>
+                                    </small>
+                                </span>
                                 <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
+                                    src="assets/img/undraw_profile.svg">
+                            </a>
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
